e826d679f617b48d05f80665f09c5595b9d209f1
Yihe224 <Yihe.Shu24@student.xjtlu.edu.cn>
2025-12-14 00:09:13 +0800

    Update Movie.java

--- src/Movie.java
+++ src/Movie.java
@@ -1,11 +1,11 @@
 // Represents one movie record loaded from CSV.
 public class Movie {
-    private final String id;
-    private final String title;
-    private final String genre;
-    private final int year;
-    private final double rating;
-
+    private final String id;// Unique identifier for the movie
+    private final String title;// Movie title
+    private final String genre;// Movie genre (e.g., Action, Comedy)
+    private final int year;// Release year
+    private final double rating;// User/ critic rating
+    //Constructor to create a Movie object with full metadata
     public Movie(String id, String title, String genre, int year, double rating) {
         this.id = id;
         this.title = title;
@@ -13,7 +13,7 @@ public class Movie {
         this.year = year;
         this.rating = rating;
     }
-
+    // Getter methods to get ID, Title, Genre, Year, Rating
     public String getId() {
         return id;
     }
